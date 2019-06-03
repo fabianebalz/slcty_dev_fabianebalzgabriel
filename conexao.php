5113217026cf1896c99a3d9e34215ca1ce424c30
fabianebalz <51092603+fabianebalz@users.noreply.github.com>
2019-06-03 14:59:42 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,10 +1,7 @@
 <?php
-/*Arquivo de conexão de banco de dados
+/*Arquivo de conexÃ£o de banco de dados
   Fabiane Balz Gabriel - 27/05/2019*/
 
-$dbstring  = "host=192.168.0.2 dbname=wagner user=postgres password=ch3vr0l3t30133";
+$dbstring  = "host=000.000.0.0 dbname=wagner user=postgres password=888888888888888";
 $conexao  = pg_connect ($dbstring);
-
-//$dbstring = "dbname=wagner user=postgres host=127.0.0.1";
-//$conexao  = pg_connect ($dbstring);
 ?>
